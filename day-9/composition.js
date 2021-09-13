52120eeb8f872a1d45b9142c2ff82f6301ef08b6
muhammad-alamin1 <muhammad.alamindev01@gmail.com>
2021-09-14 02:43:30 +0600

    day-9

---
+++ day-9/composition.js
@@ -0,0 +1,13 @@
+function printFunc(inp) {
+    console.log(inp)
+}
+
+function multiplyByFive(n) {
+    return n * 5
+}
+
+function add(a, b) {
+    return a + b
+}
+
+printFunc(multiplyByFive(add(2, 3)))
---
+++ day-9/hoisting.js
@@ -0,0 +1,13 @@
+let a = 100
+// newPrint
+
+print(10)
+
+let newPrint = print
+newPrint(45)
+
+function print(a){
+    console.log(a)
+}
+
+print(a)
